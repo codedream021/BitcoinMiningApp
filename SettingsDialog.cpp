21caa14de13e29c15d2197dcd26d6e0e5df425fa
jkadar186 <jkadar186@gmail.com>
2021-04-15 17:27:45 +0100

    Further merge after resolving conflicts

--- SettingsDialog.cpp
+++ SettingsDialog.cpp
@@ -10,11 +10,8 @@ SettingsDialog::SettingsDialog(QWidget* parent) : QDialog(parent)
 
     layout()->addWidget(loadSlider = new QSlider());
     loadSlider->setMaximum(loadSliderMax);
-<<<<<<< HEAD
     loadSlider->setValue(1);
     loadSlider->setOrientation(Qt::Horizontal);
-=======
->>>>>>> 51ea25f571f17f36eb4fb357672fc1b40b3a23e5
 
     QDialogButtonBox* buttonBox;
     layout()->addWidget(buttonBox = new QDialogButtonBox(QDialogButtonBox::Save | QDialogButtonBox::Cancel));
@@ -23,9 +20,5 @@ SettingsDialog::SettingsDialog(QWidget* parent) : QDialog(parent)
 }
 
 double SettingsDialog::getChosenLoadFraction() {
-<<<<<<< HEAD
     return loadSlider->value() / loadSliderMax;
-=======
-    return loadSlider->position() / loadSliderMax;
->>>>>>> 51ea25f571f17f36eb4fb357672fc1b40b3a23e5
 }
