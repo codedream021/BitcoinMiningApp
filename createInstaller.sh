fe4e1a280a61dfe75058185a4aaf3259af45a643
jkadar186 <jkadar186@gmail.com>
2021-05-23 22:21:45 +0200

    Updated installer/update automation script

--- createInstaller.sh
+++ createInstaller.sh
@@ -3,4 +3,9 @@
 cd ../package
 rm ../E-Pluribus-Unum-Installer.exe
 ../rar/rar a -m5 -md32m -s -sfxDefault64.SFX -z../EPU/archive-comment.txt ../E-Pluribus-Unum-Installer.exe *
+rm ../update.dat
+../rar/rar a -m5 -md32m -s -z../EPU/archive-comment.txt ../update.dat E-Pluribus-Unum.exe
 cd ../EPU
+cp ../E-Pluribus-Unum-Installer.exe ../binaries/
+cp ../update.dat ../updates/
+cp version.txt ../updates/
