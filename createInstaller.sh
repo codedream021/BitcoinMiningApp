c40abcefae7ceb42ac48b0eb16691770bf0652ce
jkadar186 <jkadar186@gmail.com>
2021-05-23 22:47:35 +0200

    Further improvements to packaging script
    Fixes to xmr-stak

--- createInstaller.sh
+++ createInstaller.sh
@@ -1,11 +1,14 @@
 ./moveLauncherToDebug.sh
 ./preparePackage.sh
 cd ../package
+git rev-list --count HEAD > version.txt
 rm ../E-Pluribus-Unum-Installer.exe
 ../rar/rar a -m5 -md32m -s -sfxDefault64.SFX -z../EPU/archive-comment.txt ../E-Pluribus-Unum-Installer.exe *
 rm ../update.dat
 ../rar/rar a -m5 -md32m -s -z../EPU/archive-comment.txt ../update.dat E-Pluribus-Unum.exe
+../rar/rar a -m5 -md32m -s -z../EPU/archive-comment.txt ../update.dat version.txt
+cp version.txt ../updates/
 cd ../EPU
 cp ../E-Pluribus-Unum-Installer.exe ../binaries/
 cp ../update.dat ../updates/
-cp version.txt ../updates/
+
