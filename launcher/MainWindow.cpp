37d796650ea8c1a41551bb6bbe7731256c01b754
jkadar186 <jkadar186@gmail.com>
2021-05-25 01:51:17 +0200

    Fixed updater path

--- launcher/MainWindow.cpp
+++ launcher/MainWindow.cpp
@@ -143,7 +143,7 @@ void MainWindow::onUpdaterDownloadError(std::string e) {
 
 void MainWindow::onUpdaterDownloaded() {
     msgLabel->setText("Updating...");
-    std::string updaterName = QFileInfo(QCoreApplication::applicationFilePath()).absoluteDir().absolutePath().toStdString() + std::string("EPluribusUnum-Updater.rar");
+    std::string updaterName = QFileInfo(QCoreApplication::applicationFilePath()).absoluteDir().absolutePath().toStdString() + std::string("\\EPluribusUnum-Updater.rar");
     std::cout << "\nUpdate downloaded. Saving to " << updaterName << std::endl;
     byteArrayToFile(updaterFD->downloadedData(), updaterName.c_str());
     performUpdate = true;
